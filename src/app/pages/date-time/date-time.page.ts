692c9ab00f01ebfaacbbaae474b7f0963fa0e2f8
Alexander Rojas <alexrrojas.b@gmail.com>
2019-03-31 23:18:41 -0600

    ion-date cap 50 next 51

--- src/app/pages/date-time/date-time.page.ts
+++ src/app/pages/date-time/date-time.page.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-date-time',
-  templateUrl: './date-time.page.html',
-  styleUrls: ['./date-time.page.scss'],
+  selector: "app-date-time",
+  templateUrl: "./date-time.page.html",
+  styleUrls: ["./date-time.page.scss"]
 })
 export class DateTimePage implements OnInit {
-
   fechaNaci: Date = new Date();
-  constructor() { }
+  customPickerOptions;
+  customDate;
+  constructor() {}
 
   ngOnInit() {
+    this.customPickerOptions = {
+      buttons: [
+        {
+          text: "Save",
+          handler: event => {
+            console.log("Clickd Save!");
+            console.log(event.day.vaue); // para obtener el dia
+          }
+        },
+        {
+          text: "Log",
+          handler: () => {
+            console.log("Clicked Log. Do not Dismiss.");
+            return false;
+          }
+        }
+      ]
+    };
   }
   cambioFecha(event) {
     console.log(event);
